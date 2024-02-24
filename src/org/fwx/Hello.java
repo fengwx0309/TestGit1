039b2a74ac17c8b0623ffa5c4a5b8279a53c2917
fengwx0309 <160861850+fengwx0309@users.noreply.github.com>
2024-02-24 21:00:10 +0800

    Update Hello.java

--- src/org/fwx/Hello.java
+++ src/org/fwx/Hello.java
@@ -14,5 +14,6 @@ public class Hello {
         System.out.println("master");
         System.out.println("hot-fix");
         System.out.println("push-test");
+        System.out.println("pull-test");
     }
 }
