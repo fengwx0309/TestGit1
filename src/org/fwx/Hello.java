bd8259df7501dd2a68aab22b1a9bc71677fa7e02
fwx <345025106@qq.com>
2024-02-24 19:38:53 +0800

    master

--- src/org/fwx/Hello.java
+++ src/org/fwx/Hello.java
@@ -11,5 +11,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("hello1");
         System.out.println("hello2");
+        System.out.println("master");
     }
 }
